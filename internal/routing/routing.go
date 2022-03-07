27e307dee4eb5e0edea58ce2c505afb1fe591f8c
Morre <morre@mor.re>
2022-03-07 11:13:40 +0100

    feat: support HTTP OPTIONS verb

--- internal/routing/routing.go
+++ internal/routing/routing.go
@@ -12,20 +12,36 @@ import (
 func Router() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
-	})
-
 	database.ConnectDatabase()
 
+	// The root path lists the available versions
+	r.GET("", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"v1": "/v1",
+		})
+	})
+
+	// Options lists the allowed HTTP verbs
+	r.OPTIONS("", func(c *gin.Context) {
+		c.Header("allow", "GET")
+	})
+
+	// API v1 setup
 	v1 := r.Group("/v1")
 	{
-		v1.GET("/budgets", controllers.GetBudgets)
-		v1.GET("/budgets/:id", controllers.GetBudget)
-		v1.POST("/budgets", controllers.CreateBudget)
-		v1.PATCH("budgets/:id", controllers.UpdateBudget)
-		v1.DELETE("budgets/:id", controllers.DeleteBudget)
+		v1.GET("", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"budgets": "/budgets",
+			})
+		})
+
+		v1.OPTIONS("", func(c *gin.Context) {
+			c.Header("allow", "GET")
+		})
 	}
+
+	budgets := v1.Group("/budgets")
+	controllers.RegisterBudgetRoutes(budgets)
 
 	return r
 }
