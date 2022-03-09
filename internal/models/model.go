debc62d25fb7bfbd80b0bb9a2628a13645fa8439
Morre <morre@mor.re>
2022-03-09 11:16:32 +0100

    feat: add DeletedAt key to base model for soft-delete support

--- internal/models/model.go
+++ internal/models/model.go
@@ -1,10 +1,15 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 // Model is the base model for all other models in Envelope Zero
 type Model struct {
-	ID        uint      `json:"id"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+	ID        uint           `json:"id"`
+	CreatedAt time.Time      `json:"createdAt"`
+	UpdatedAt time.Time      `json:"updatedAt"`
+	DeletedAt gorm.DeletedAt `json:"deletedAt" gorm:"index"`
 }
