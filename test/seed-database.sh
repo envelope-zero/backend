91e50b4de3f6c7f5f610e905d4ba9ae2f1a3e418
Morre <morre@mor.re>
2022-03-11 20:37:59 +0100

    test: add database seeding script

---
+++ test/seed-database.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+http post localhost:8080/v1/budgets name=Morre
+http post localhost:8080/v1/budgets/1/accounts name='Bank Account' visible:=true onBudget:=true
+http post localhost:8080/v1/budgets/1/accounts name=Cash visible:=true onBudget:=true
+http post localhost:8080/v1/budgets/1/accounts name=Netflix
+
+http post localhost:8080/v1/budgets/1/transactions amount='17.99' sourceAccountId:=1 destinationAccountId:=3 date='2022-02-25T00:00:00Z'
+http post localhost:8080/v1/budgets/1/transactions amount='17.99' sourceAccountId:=1 destinationAccountId:=3 date='2022-01-25T00:00:00Z'
+http post localhost:8080/v1/budgets/1/transactions amount='17.99' sourceAccountId:=1 destinationAccountId:=3 date='2021-12-27T00:00:00Z'
+http post localhost:8080/v1/budgets/1/transactions amount='17.99' sourceAccountId:=1 destinationAccountId:=3 date='2021-11-25T00:00:00Z'
+http post localhost:8080/v1/budgets/1/transactions amount='17.99' sourceAccountId:=1 destinationAccountId:=3 date='2021-10-26T00:00:00Z'
