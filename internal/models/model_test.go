3342abfd5140307611e9a3c0c84b4fddd336bd4b
Morre <morre@mor.re>
2022-04-01 17:36:12 +0200

    test: add tests for models

---
+++ internal/models/model_test.go
@@ -0,0 +1,29 @@
+package models_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/envelope-zero/backend/internal/models"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+)
+
+func TestModelTimeUTC(t *testing.T) {
+	tz, _ := time.LoadLocation("Europe/Berlin")
+
+	model := models.Model{
+		CreatedAt: time.Date(2000, 1, 2, 3, 4, 5, 6, tz),
+		UpdatedAt: time.Date(2001, 2, 3, 4, 5, 6, 7, tz),
+		DeletedAt: &gorm.DeletedAt{Time: time.Now().In(tz)},
+	}
+
+	err := model.AfterFind(models.DB)
+	if err != nil {
+		assert.Fail(t, "model.AfterFind failed")
+	}
+
+	assert.Equal(t, time.UTC, model.CreatedAt.Location(), "Timezone for model is not UTC")
+	assert.Equal(t, time.UTC, model.UpdatedAt.Location(), "Timezone for model is not UTC")
+	assert.Equal(t, time.UTC, model.DeletedAt.Time.Location(), "Timezone for model is not UTC")
+}
