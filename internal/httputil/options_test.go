ff22cbe4f7c6accf71d7281111d9fee73a46cd26
Morre <morre@mor.re>
2022-05-17 14:07:53 +0200

    test: add tests for httputil

---
+++ internal/httputil/options_test.go
@@ -0,0 +1,56 @@
+package httputil_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/envelope-zero/backend/internal/httputil"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOptionsGet(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", httputil.OptionsGet)
+
+	// Check without reverse proxy headers
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, "GET", w.Header().Get("allow"))
+	assert.Equal(t, http.StatusNoContent, w.Code)
+}
+
+func TestOptionsPost(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", httputil.OptionsGetPost)
+
+	// Check without reverse proxy headers
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, "GET, POST", w.Header().Get("allow"))
+	assert.Equal(t, http.StatusNoContent, w.Code)
+}
+
+func TestOptionsGetPatchDelete(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", httputil.OptionsGetPatchDelete)
+
+	// Check without reverse proxy headers
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, "GET, PATCH, DELETE", w.Header().Get("allow"))
+	assert.Equal(t, http.StatusNoContent, w.Code)
+}
--- internal/httputil/request_test.go
+++ internal/httputil/request_test.go
@@ -1,11 +1,14 @@
 package httputil_test
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/envelope-zero/backend/internal/httputil"
+	"github.com/envelope-zero/backend/internal/test"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
@@ -56,4 +59,153 @@ func TestRequestHostPrefix(t *testing.T) {
 	c.Request.Header.Set("x-forwarded-prefix", "/backend")
 	r.ServeHTTP(w, c.Request)
 	assert.Equal(t, "http://example.com/backend", w.Body.String())
+}
+
+func TestRequestHostProtoHTTPS(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", func(ctx *gin.Context) {
+		c.String(http.StatusOK, httputil.RequestHost(c))
+	})
+
+	// Check with x-forwarded-prefix
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	c.Request.Host = "::1"
+	c.Request.Header.Set("x-forwarded-host", "example.com")
+	c.Request.Header.Set("x-forwarded-proto", "https")
+	r.ServeHTTP(w, c.Request)
+	assert.Equal(t, "https://example.com/api", w.Body.String())
+}
+
+func TestRequestPathV1(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", func(ctx *gin.Context) {
+		c.String(http.StatusOK, httputil.RequestPathV1(c))
+	})
+
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+	assert.Equal(t, "http://example.com/v1", w.Body.String())
+}
+
+func TestRequestPathURI(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/seenotrettung/ist-kein-verbrechen", func(ctx *gin.Context) {
+		c.String(http.StatusOK, httputil.RequestURL(c))
+	})
+
+	c.Request, _ = http.NewRequest(http.MethodGet, "/seenotrettung/ist-kein-verbrechen", nil)
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+	assert.Equal(t, "http://example.com/seenotrettung/ist-kein-verbrechen", w.Body.String())
+}
+
+func TestParseIDInvalid(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", func(ctx *gin.Context) {
+		id, err := httputil.ParseID(c, "NotAValidUint")
+		if err != nil {
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("%d", id))
+	})
+
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	// Not checking the error messages here as this is done in error_test.go
+	assert.Equal(t, http.StatusBadRequest, w.Code)
+}
+
+func TestParseID(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+	c.Params = append(c.Params, gin.Param{Key: "id", Value: "171"})
+
+	r.GET("/", func(ctx *gin.Context) {
+		id, err := httputil.ParseID(c, "id")
+		if err != nil {
+			return
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("%d", id))
+	})
+
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", nil)
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, "171", w.Body.String())
+}
+
+func TestBindData(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", func(ctx *gin.Context) {
+		var o struct {
+			Name string `json:"name"`
+		}
+
+		_ = httputil.BindData(c, &o)
+	})
+
+	// Check without reverse proxy headers
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", bytes.NewBuffer([]byte(`{ "name": "Drink more water!" }`)))
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, http.StatusOK, w.Code, "Binding failed: %s", w.Body.String())
+}
+
+func TestBindBrokenData(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", func(ctx *gin.Context) {
+		var o struct {
+			Name string `json:"name"`
+		}
+
+		_ = httputil.BindData(c, &o)
+	})
+
+	// Check without reverse proxy headers
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", bytes.NewBuffer([]byte(`{ broken json: "Drink more water!" }`)))
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code, "Binding failed: %s", w.Body.String())
+	assert.Contains(t, test.DecodeError(t, w.Body.Bytes()), "the body of your request contains invalid or un-parseable data")
+}
+
+func TestBindEmptyBody(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, r := gin.CreateTestContext(w)
+
+	r.GET("/", func(ctx *gin.Context) {
+		var o struct {
+			Name string `json:"name"`
+		}
+
+		_ = httputil.BindData(c, &o)
+	})
+
+	// Check without reverse proxy headers
+	c.Request, _ = http.NewRequest(http.MethodGet, "/", bytes.NewBuffer([]byte("")))
+	c.Request.Host = "example.com"
+	r.ServeHTTP(w, c.Request)
+
+	assert.Equal(t, http.StatusBadRequest, w.Code, "Binding failed: %s", w.Body.String())
+	assert.Contains(t, test.DecodeError(t, w.Body.Bytes()), "request body must not be emtpy")
 }
