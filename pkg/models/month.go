d2346e9e3b47a644ca91140f9c301d09288a116a
Morre <morre@mor.re>
2022-09-20 21:12:26 +0200

    feat: add envelope balance sum to month response (#363)

--- pkg/models/month.go
+++ pkg/models/month.go
@@ -17,8 +17,9 @@ type Month struct {
 	ID         uuid.UUID           `json:"id" example:"1e777d24-3f5b-4c43-8000-04f65f895578"` // The ID of the Budget
 	Name       string              `json:"name" example:"Zero budget"`                        // The name of the Budget
 	Month      time.Time           `json:"month" example:"2006-05-01T00:00:00.000000Z"`       // This is always set to 00:00 UTC on the first of the month.
-	Budgeted   decimal.Decimal     `json:"budgeted" example:"2100"`
-	Income     decimal.Decimal     `json:"income" example:"2317.34"`
-	Available  decimal.Decimal     `json:"available" example:"217.34"`
-	Categories []CategoryEnvelopes `json:"categories"`
+	Budgeted   decimal.Decimal     `json:"budgeted" example:"2100"`                           // The sum of all allocations for the month
+	Income     decimal.Decimal     `json:"income" example:"2317.34"`                          // The total income for the month (sum of all incoming transactions without an Envelope)
+	Available  decimal.Decimal     `json:"available" example:"217.34"`                        // The amount available to budget
+	Balance    decimal.Decimal     `json:"balance" example:"5231.37"`                         // The sum of all envelope balances
+	Categories []CategoryEnvelopes `json:"categories"`                                        // A list of envelope month calculations grouped by category
 }
