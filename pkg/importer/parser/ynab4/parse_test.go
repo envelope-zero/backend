eab29671bbf1aba9674e5a2e7131a30521662249
Morre <morre@mor.re>
2022-12-31 20:24:44 +0100

    test: add tests for invalid and erroring files

--- pkg/importer/parser/ynab4/parse_test.go
+++ pkg/importer/parser/ynab4/parse_test.go
@@ -1,12 +1,20 @@
 package ynab4_test
 
 import (
+	"errors"
 	"os"
 	"testing"
+	"testing/iotest"
 
 	"github.com/envelope-zero/backend/pkg/importer/parser/ynab4"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestParseNoFile(t *testing.T) {
+	_, err := ynab4.Parse(iotest.ErrReader(errors.New("Some reading error")))
+	assert.NotNil(t, err, "Expected file opening to fail")
+	assert.Contains(t, err.Error(), "could not read data from file", "Wrong error on parsing broken file: %s", err)
+}
 
 func TestParse(t *testing.T) {
 	f, err := os.OpenFile("../../../../testdata/Budget.yfull", os.O_RDONLY, 0o400)
@@ -15,7 +23,16 @@ func TestParse(t *testing.T) {
 	}
 
 	_, err = ynab4.Parse(f)
+	assert.Nil(t, err, "Parsing failed", err)
+}
+
+func TestParseBrokenFile(t *testing.T) {
+	f, err := os.OpenFile("../../../../testdata/EmptyFile.yfull", os.O_RDONLY, 0o400)
 	if err != nil {
-		assert.Fail(t, "Parsing failed", err)
+		assert.FailNow(t, "Failed to open the test file", err)
 	}
+
+	_, err = ynab4.Parse(f)
+	assert.NotNil(t, err, "Expected parsing to fail")
+	assert.Contains(t, err.Error(), "not a valid YNAB4 Budget.yfull file", "Wrong error on parsing broken file: %s", err)
 }
