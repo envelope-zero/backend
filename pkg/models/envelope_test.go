8a9204928c88e3b81f461d181d20c49b7d332d4e
Morre <morre@mor.re>
2023-01-12 10:20:50 +0100

    test: improve envelope month balance test maintainability

--- pkg/models/envelope_test.go
+++ pkg/models/envelope_test.go
@@ -1,6 +1,8 @@
 package models_test
 
 import (
+	"fmt"
+	"testing"
 	"time"
 
 	"github.com/envelope-zero/backend/v2/internal/types"
@@ -166,143 +168,86 @@ func (suite *TestSuiteStandard) TestCreateTransactionNoEnvelope() {
 	assert.Nil(suite.T(), err, "Transactions must be able to be created without an envelope (to enable internal transfers without an Envelope and income transactions)")
 }
 
+// TestEnvelopeMonthBalance verifies that the monthly balance calculation for
+// envelopes is correct.
 func (suite *TestSuiteStandard) TestEnvelopeMonthBalance() {
-	budget := models.Budget{}
-	err := suite.db.Save(&budget).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
+	budget := suite.createTestBudget(models.BudgetCreate{})
 
-	internalAccount := &models.Account{
-		AccountCreate: models.AccountCreate{
-			Name:     "Internal Source Account",
-			BudgetID: budget.ID,
-		},
-	}
-	err = suite.db.Create(internalAccount).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
+	internalAccount := suite.createTestAccount(models.AccountCreate{
+		Name:     "Internal Source Account",
+		BudgetID: budget.ID,
+	})
 
-	externalAccount := &models.Account{
-		AccountCreate: models.AccountCreate{
-			Name:     "External Destination Account",
-			BudgetID: budget.ID,
-			External: true,
-		},
-	}
-	err = suite.db.Create(&externalAccount).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
+	externalAccount := suite.createTestAccount(models.AccountCreate{
+		Name:     "External Destination Account",
+		BudgetID: budget.ID,
+		External: true,
+	})
 
-	category := models.Category{
-		CategoryCreate: models.CategoryCreate{
-			BudgetID: budget.ID,
-		},
-	}
-	err = suite.db.Save(&category).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
+	category := suite.createTestCategory(models.CategoryCreate{
+		BudgetID: budget.ID,
+	})
 
-	envelope := &models.Envelope{
-		EnvelopeCreate: models.EnvelopeCreate{
-			Name:       "Testing envelope",
-			CategoryID: category.ID,
-		},
-	}
-	err = suite.db.Create(&envelope).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
+	envelope := suite.createTestEnvelope(models.EnvelopeCreate{
+		Name:       "Testing envelope",
+		CategoryID: category.ID,
+	})
 
 	// Used to test the Envelope.Balance method without any transactions
-	envelope2 := &models.Envelope{
-		EnvelopeCreate: models.EnvelopeCreate{
-			Name:       "Testing envelope without any transactions",
-			CategoryID: category.ID,
-		},
-	}
-	err = suite.db.Create(&envelope2).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
+	envelopeWithoutTransactions := suite.createTestEnvelope(models.EnvelopeCreate{
+		Name:       "Testing envelope without any transactions",
+		CategoryID: category.ID,
+	})
 
 	january := types.NewMonth(2022, 1)
 
-	allocationJan := &models.Allocation{
-		AllocationCreate: models.AllocationCreate{
-			EnvelopeID: envelope.ID,
-			Month:      january,
-			Amount:     decimal.NewFromFloat(50),
-		},
-	}
-	err = suite.db.Create(&allocationJan).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
+	_ = suite.createTestAllocation(models.AllocationCreate{
+		EnvelopeID: envelope.ID,
+		Month:      january,
+		Amount:     decimal.NewFromFloat(50),
+	})
+
+	_ = suite.createTestAllocation(models.AllocationCreate{
+		EnvelopeID: envelope.ID,
+		Month:      january.AddDate(0, 1),
+		Amount:     decimal.NewFromFloat(40),
+	})
+
+	_ = suite.createTestTransaction(models.TransactionCreate{
+		BudgetID:             budget.ID,
+		EnvelopeID:           &envelope.ID,
+		Amount:               decimal.NewFromFloat(15),
+		SourceAccountID:      internalAccount.ID,
+		DestinationAccountID: externalAccount.ID,
+		Date:                 time.Time(january),
+	})
+
+	_ = suite.createTestTransaction(models.TransactionCreate{
+		BudgetID:             budget.ID,
+		EnvelopeID:           &envelope.ID,
+		Amount:               decimal.NewFromFloat(30),
+		SourceAccountID:      internalAccount.ID,
+		DestinationAccountID: externalAccount.ID,
+		Date:                 time.Time(january.AddDate(0, 1)),
+	})
+
+	tests := []struct {
+		month    types.Month
+		envelope models.Envelope
+		balance  float32
+	}{
+		{january, envelope, 35},
+		{january.AddDate(0, 1), envelope, 45},
+		{types.NewMonth(2022, 12), envelope, 45}, // Verify balance for December (regression test for using AddDate(0, 1, 0) with the month instead of the whole date)
+		{january.AddDate(0, 1), envelopeWithoutTransactions, 0},
 	}
 
-	allocationFeb := &models.Allocation{
-		AllocationCreate: models.AllocationCreate{
-			EnvelopeID: envelope.ID,
-			Month:      january.AddDate(0, 1),
-			Amount:     decimal.NewFromFloat(40),
-		},
+	for _, tt := range tests {
+		suite.T().Run(fmt.Sprintf("%s-%s", tt.envelope.Name, tt.month.String()), func(t *testing.T) {
+			should := decimal.NewFromFloat(float64(tt.balance))
+			eMonth, _, err := tt.envelope.Month(suite.db, tt.month)
+			assert.Nil(t, err)
+			assert.True(t, eMonth.Balance.Equal(should), "Balance calculation for 2022-01 is wrong: should be %v, but is %v", should, eMonth.Balance)
+		})
 	}
-	err = suite.db.Create(&allocationFeb).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
-
-	transaction := &models.Transaction{
-		TransactionCreate: models.TransactionCreate{
-			BudgetID:             budget.ID,
-			EnvelopeID:           &envelope.ID,
-			Amount:               decimal.NewFromFloat(15),
-			SourceAccountID:      internalAccount.ID,
-			DestinationAccountID: externalAccount.ID,
-			Date:                 time.Time(january),
-		},
-	}
-	err = suite.db.Create(&transaction).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
-
-	transaction2 := &models.Transaction{
-		TransactionCreate: models.TransactionCreate{
-			BudgetID:             budget.ID,
-			EnvelopeID:           &envelope.ID,
-			Amount:               decimal.NewFromFloat(30),
-			SourceAccountID:      internalAccount.ID,
-			DestinationAccountID: externalAccount.ID,
-			Date:                 time.Time(january.AddDate(0, 1)),
-		},
-	}
-	err = suite.db.Create(&transaction2).Error
-	if err != nil {
-		suite.Assert().Fail("Resource could not be saved", err)
-	}
-
-	shouldBalance := decimal.NewFromFloat(35)
-	envelopeMonth, _, err := envelope.Month(suite.db, january)
-	assert.Nil(suite.T(), err)
-	assert.True(suite.T(), envelopeMonth.Balance.Equal(shouldBalance), "Balance calculation for 2022-01 is wrong: should be %v, but is %v", shouldBalance, envelopeMonth.Balance)
-
-	shouldBalance = decimal.NewFromFloat(45)
-	envelopeMonth, _, err = envelope.Month(suite.db, january.AddDate(0, 1))
-	assert.Nil(suite.T(), err)
-	assert.True(suite.T(), envelopeMonth.Balance.Equal(shouldBalance), "Balance calculation for 2022-02 is wrong: should be %v, but is %v", shouldBalance, envelopeMonth.Balance)
-
-	// Verify balance for December (regression test for using AddDate(0, 1, 0) with the month instead of the whole date)
-	shouldBalance = decimal.NewFromFloat(45)
-	envelopeMonth, _, err = envelope.Month(suite.db, types.NewMonth(2022, 12))
-	assert.Nil(suite.T(), err)
-	assert.True(suite.T(), envelopeMonth.Balance.Equal(shouldBalance), "Balance calculation for 2022-02 is wrong: should be %v, but is %v", shouldBalance, envelopeMonth.Balance)
-
-	shouldBalance = decimal.NewFromFloat(0)
-	envelopeMonth, _, err = envelope2.Month(suite.db, january.AddDate(0, 1))
-	assert.Nil(suite.T(), err)
-	assert.True(suite.T(), envelopeMonth.Balance.Equal(shouldBalance), "Balance calculation for 2022-02 is wrong: should be %v, but is %v", shouldBalance, envelopeMonth.Balance)
 }
