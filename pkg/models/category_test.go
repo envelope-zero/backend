12237edf99b583c81ed2fb2f295dbed623469ebd
Max Rosin <git@hackrid.de>
2023-11-18 22:55:06 +0100

    fix: only archive envelopes in category when archiving a category (#846)

--- pkg/models/category_test.go
+++ pkg/models/category_test.go
@@ -20,10 +20,37 @@ func (suite *TestSuiteStandard) TestCategoryArchiveArchivesEnvelopes() {
 	err := suite.db.Model(&category).Select("Hidden").Updates(models.Category{CategoryCreate: models.CategoryCreate{Hidden: true}}).Error
 	assert.Nil(suite.T(), err)
 
-	// Verify that the envelope is not archived
+	// Verify that the envelope is archived
 	err = suite.db.First(&envelope, envelope.ID).Error
 	assert.Nil(suite.T(), err)
 	assert.True(suite.T(), envelope.Hidden, "Envelope was not archived together with category")
+}
+
+func (suite *TestSuiteStandard) TestCategoryArchiveNoEnvelopes() {
+	budget := suite.createTestBudget(models.BudgetCreate{})
+	category := suite.createTestCategory(models.CategoryCreate{
+		BudgetID: budget.ID,
+		Name:     "TestCategoryArchiveNoEnvelopes",
+	})
+
+	category2 := suite.createTestCategory(models.CategoryCreate{
+		BudgetID: budget.ID,
+	})
+
+	envelope := suite.createTestEnvelope(models.EnvelopeCreate{
+		CategoryID: category2.ID,
+		Hidden:     false,
+	})
+	assert.False(suite.T(), envelope.Hidden, "Envelope archived on creation, it should not be")
+
+	// Archive the empty category
+	err := suite.db.Model(&category).Select("Hidden").Updates(models.Category{CategoryCreate: models.CategoryCreate{Hidden: true}}).Error
+	assert.Nil(suite.T(), err)
+
+	// Verify that the envelope is not archived
+	err = suite.db.First(&envelope, envelope.ID).Error
+	assert.Nil(suite.T(), err)
+	assert.False(suite.T(), envelope.Hidden, "Envelope was archived together with category")
 }
 
 func (suite *TestSuiteStandard) TestCategorySetEnvelopes() {
