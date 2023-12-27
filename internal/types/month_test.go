0d1aeb16282b8661148f72a32266944a2b585728
Morre <morre@mor.re>
2023-12-28 00:09:50 +0100

    fix: Month JSON unmarshaling now enforcing year and month only
    This fixes an issue where a variable of type types.Month could have
    other parts than just year and month set, which was possible when
    parsing it from user input.
    
    This never surfaced since this has not been unmarshaled from user input yet
    where the input could contain this.
    
    However, this will be the case starting with goals.
    Therefore, this bug is fixed now.

---
+++ internal/types/month_test.go
@@ -0,0 +1,21 @@
+package types_test
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/envelope-zero/backend/v3/internal/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMonthUnmarshalJSON(t *testing.T) {
+	var target struct {
+		Month types.Month
+	}
+	jsonString := []byte(`{ "month": "2024-05-12T17:59:23+02:00" }`)
+
+	err := json.Unmarshal(jsonString, &target)
+
+	assert.Nil(t, err)
+	assert.Equal(t, types.NewMonth(2024, 5), target.Month)
+}
