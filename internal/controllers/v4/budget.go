c74a11081c5cd2f174ca7a82ecd47dbbf7cffbfd
Morre <morre@mor.re>
2025-03-07 10:47:23 +0100

    refactor: use generics for resource detail OPTIONS requests

--- internal/controllers/v4/budget.go
+++ internal/controllers/v4/budget.go
@@ -47,24 +47,7 @@ func OptionsBudgetList(c *gin.Context) {
 // @Param			id	path		URIID	true	"ignored, but needed: https://github.com/swaggo/swag/issues/1014"
 // @Router			/v4/budgets/{id} [options]
 func OptionsBudgetDetail(c *gin.Context) {
-	var uri URIID
-	err := c.ShouldBindUri(&uri)
-	if err != nil {
-		c.JSON(status(err), httpError{
-			Error: err.Error(),
-		})
-		return
-	}
-
-	err = models.DB.First(&models.Budget{}, uri.ID).Error
-	if err != nil {
-		c.JSON(status(err), httpError{
-			Error: err.Error(),
-		})
-		return
-	}
-
-	httputil.OptionsGetPatchDelete(c)
+	resourceOptionsDetail(c, models.Budget{})
 }
 
 // @Summary		Create budget
