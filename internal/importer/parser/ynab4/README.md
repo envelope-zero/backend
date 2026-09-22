3d415f968fc9053d83b66b1fbc96b91d1bbf8418
Morre <morre@mor.re>
2026-09-23 01:18:06 +0200

    docs: archival notice

--- internal/importer/parser/ynab4/README.md
+++
@@ -1,3 +0,0 @@
-# YNAB 4 parser
-
-Import hashes for YNAB 4 always use the entity ID from YNAB 4.
--- pkg/db/db.go
+++
@@ -1,7 +0,0 @@
-package db
-
-import "github.com/envelope-zero/backend/v7/internal/models"
-
-func Connect(dsn string) error {
-	return models.Connect(dsn)
-}
